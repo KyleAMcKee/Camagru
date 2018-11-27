26b496f19dda82200bb9b01091ed99925c558aa0
KyleAMcKee <kyleandrewmckee@gmail.com>
2018-11-26 17:35:39 -0800

    profile updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 import store from './store';
 
 import Navbar from './components/layout/Navbar';
@@ -11,6 +12,8 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/common/PrivateRoute';
 
 import './App.css';
 
@@ -22,7 +25,8 @@ if (localStorage.jwtToken) {
 	// Check for expired token
 	const currentTime = Date.now() / 1000;
 	if (currentTime > decoded.exp) {
-		store.dispatch(logoutUser);
+		store.dispatch(logoutUser());
+		store.dispatch(clearCurrentProfile());
 		window.location.href = '/login';
 	}
 }
@@ -38,6 +42,9 @@ class App extends Component {
 						<div className="container">
 							<Route exact path="/register" component={ Register } />
 							<Route exact path="/login" component={ Login } />
+							<Switch>
+								<PrivateRoute exact path="/dashboard" component={ Dashboard } />
+							</Switch>
 						</div>
 						<Footer />
 					</div>
